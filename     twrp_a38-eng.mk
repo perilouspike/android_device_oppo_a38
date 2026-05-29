1b6303e82f45eda0085743780a630a81969a0638
perilouspike <97983211+perilouspike@users.noreply.github.com>
2026-05-29 17:33:44 +0530

    CPH2579: Initial tree brought up.

---
+++     twrp_a38-eng.mk
@@ -0,0 +1,32 @@
+#
+# Copyright (C) 2026 The Android Open Source Project
+# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product-if-exists, vendor/twrp/config/common.mk)
+
+# Inherit from PBRP-common stuff, if building PBRP.
+$(call inherit-product-if-exists, vendor/pb/config/common.mk)
+
+# Inherit from a38 device
+$(call inherit-product, device/Oppo/A38/device.mk)
+
+PRODUCT_DEVICE := A38
+PRODUCT_NAME := twrp_a38
+PRODUCT_BRAND := Oppo
+PRODUCT_MODEL := A38
+PRODUCT_MANUFACTURER := Oppo
+
+PRODUCT_GMS_CLIENTID_BASE := android-oppo
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="vnd_k69v1_64_k419-user 12 SP1A.210812.016 1733193326975 release-keys"
+
+BUILD_FINGERPRINT := oplus/ossi/ossi:12/SP1A.210812.016/1733193326975:user/release-keys
---
+++ AndroidProducts.mk
@@ -0,0 +1,12 @@
+#
+# Copyright (C) 2026 The Android Open Source Project
+# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_a38.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_a38-eng
