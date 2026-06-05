816ccedbd715583349945206e73747cde160e58f
Shibu Shaji <shibushaji199818@proton.me>
2026-06-06 02:36:29 +0530

    Update twrp_A38.mk
    Fix build error, explicitly inherited.

--- twrp_A38.mk
+++ twrp_A38.mk
@@ -10,10 +10,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product-if-exists, vendor/twrp/config/common.mk)
-
-# Inherit from ossi device
-$(call inherit-product, device/oppo/ossi/device.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from a38 device
 $(call inherit-product, device/Oppo/A38/device.mk)
