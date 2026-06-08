c86a644545d3affac606da105527a4791da55d07
perilouspike <shibushaji199818@proton.me>
2026-06-08 22:56:20 +0530

    Update BoardConfig.mk with Android 14 flags and additional boot partitions
    Signed-off-by: perilouspike <shibushaji199818@proton.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,6 +9,14 @@ DEVICE_PATH := device/Oppo/A38
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
+BUILD_BROKEN_DUP_RULES := true
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+
+# Android 14 Specific Flags
+# Network
+BUILD_BROKEN_USES_NETWORK := true
+BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
+BUILD_BROKEN_CLANG_PROPERTY_DICTIONARY_CHECKS := true
 
 # A/B
 AB_OTA_UPDATER := true
@@ -18,7 +26,11 @@ AB_OTA_PARTITIONS += \
     vbmeta_vendor \
     system \
     vbmeta_system \
-    product
+    product \
+    boot \
+    dtbo \
+    lk \
+    preloader
 
 # Architecture
 TARGET_ARCH := arm64
@@ -26,14 +38,17 @@ TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 := 
 TARGET_CPU_VARIANT := generic
-TARGET_CPU_VARIANT_RUNTIME := cortex-a53
+TARGET_CPU_VARIANT_RUNTIME := cortex-a75
 
 TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv7-a-neon
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := generic
-TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a53
+TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a55
+
+ENABLE_CPUSETS := true
+ENABLE_SCHEDBOOST := true
 
 # APEX
 DEXPREOPT_GENERATE_APEX_IMAGE := true
@@ -41,6 +56,9 @@ DEXPREOPT_GENERATE_APEX_IMAGE := true
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := k69v1_64_k419
 TARGET_NO_BOOTLOADER := true
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := CPH2579,cph2579,CPH2581,cph2581
 
 # Display
 TARGET_SCREEN_DENSITY := 480
